2f779176aabc1938b8ed41381269dd10eb516196
Tomáš Kárník <49093071+TomasKarnik@users.noreply.github.com>
2024-05-23 15:13:35 -0700

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== TRUE) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>User login system</title>
+  <title>StarGejt</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <link rel="stylesheet" href="./css/main.css">
   <link rel="shortcut icon" href="./img/favicon-16x16.png" type="image/x-icon">
@@ -35,6 +35,9 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== TRUE) {
         <h4 class="my-4">Hello, <?= htmlspecialchars($_SESSION["username"]); ?></h4>
         <a href="./upload_form.php" class="btn btn-primary">Zapiš průjezd</a>
         <a href="./main.php" class="btn btn-primary">Záznamy průjezdů</a>
+        <a href="./company.php" class="btn btn-primary">Firmy</a>
+        <a href="./persons.php" class="btn btn-primary">Lidé</a>
+        <a href="./spz.php" class="btn btn-primary">Vozidla</a>
         <a href="./logout.php" class="btn btn-primary">Log Out</a>
       </div>
     </div>
