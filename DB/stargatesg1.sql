28060bfa829c91e146b60837d0416feec7341467
Tomáš Kárník <49093071+TomasKarnik@users.noreply.github.com>
2024-05-23 13:44:28 +0200

    sql

---
+++ DB/stargatesg1.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Počítač: 127.0.0.1
+-- Vytvořeno: Čtv 23. kvě 2024, 13:43
+-- Verze serveru: 10.4.27-MariaDB
+-- Verze PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databáze: `stargatesg1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `stargate_data`
+--
+
+CREATE TABLE `stargate_data` (
+  `id` int(11) NOT NULL,
+  `spz` varchar(50) DEFAULT NULL,
+  `saved_date` date DEFAULT NULL,
+  `picture` mediumblob DEFAULT NULL,
+  `saved_time` datetime DEFAULT NULL,
+  `firma` varchar(255) DEFAULT NULL,
+  `osoba` varchar(255) DEFAULT NULL,
+  `typ` varchar(255) DEFAULT NULL,
+  `poznámka` varchar(255) DEFAULT NULL,
+  `důvod` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `reg_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexy pro exportované tabulky
+--
+
+--
+-- Indexy pro tabulku `stargate_data`
+--
+ALTER TABLE `stargate_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexy pro tabulku `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pro tabulky
+--
+
+--
+-- AUTO_INCREMENT pro tabulku `stargate_data`
+--
+ALTER TABLE `stargate_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT pro tabulku `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
