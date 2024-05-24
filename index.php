185e3310c62e0bd23022001c54c993af75096d80
Tomáš Kárník <49093071+TomasKarnik@users.noreply.github.com>
2024-05-24 09:13:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,13 +19,13 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== TRUE) {
   <title>StarGejt</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <link rel="stylesheet" href="./css/main.css">
-  <link rel="shortcut icon" href="./img/favicon-16x16.png" type="image/x-icon">
+  <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
 </head>
 
 <body>
   <div class="container">
     <div class="alert alert-success my-5">
-    <img src="./img/logo-spolecnosti.png" class="img-fluid rounded" alt="Logo" width="180">
+    <img src="./img/logo.png" class="img-fluid rounded" alt="Logo" width="180">
       Welcome ! You are now signed in to your account.
     </div>
     <!-- User profile -->
